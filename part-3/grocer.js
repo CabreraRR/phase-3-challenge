5813170ac9167e76528a84d0ca80d4437d688475
Roel Cabrera <roel.cabrera@gmail.com>
2017-08-07 15:44:39 -0700

    progress on listing of cart contents

--- part-3/grocer.js
+++ part-3/grocer.js
@@ -4,42 +4,47 @@ const span = document.getElementsByClassName('close')[0];
 const clear = document.getElementById('clear');
 const add = document.getElementById('add');
 const cart = document.getElementById('cart-item-count');
+const contents = document.getElementById('list');
 
-
-
-let cartContents =[];
+let cartContents = [];
 let cartNum = 0;
 
+test = "test"
 window.onload = itemCount();
-
 
 
 function itemCount() {
     cart.innerHTML = "(" + cartNum + ")";
-    if (cartNum === 0){
-    	console.log('Your cart is empty :(');
+    if (cartNum === 0) {
+        console.log('Your cart is empty :(');
     } else {
-    	console.log('Your item has been added to your cart');
+        console.log('Your item has been added to your cart');
     }
 };
 
+
+
 add.onclick = function() {
-	let item = {};
-	item.name = add.previousElementSibling.previousElementSibling.innerHTML;
-	item.price = add.previousElementSibling.innerHTML;
-	cartContents.push(item);
-	cartNum ++
-	itemCount();
+    let item = {};
+    cartContents.push(item);
+    cartNum++;
+    itemCount();
 }
+
+function printCart() {
+	createli();
+};
 
 btn.onclick = function() {
     modal.style.display = "block";
-}
 
+}
 span.onclick = function() {
     modal.style.display = "none";
 }
 
 clear.onclick = function() {
-
+    cartContents = [];
+    cartNum = 0;
+    itemCount();
 }
