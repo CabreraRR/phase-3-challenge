e341ef379a8ff7a63d8195b292fecbf45d514f75
Roel Cabrera <roel.cabrera@gmail.com>
2017-08-07 12:32:40 -0700

    applied modal and basic functionality of a modal open/close

--- part-3/grocer.js
+++ part-3/grocer.js
@@ -1,12 +1,25 @@
-let cartNum = 0;
+let cartNum = cart.length;
+let cart = [ ];
 
-window.onload= itemCount();
+window.onload = itemCount();
 
 function itemCount() {
-	document.getElementById('cart-item-count').innerHTML = "(" + cartNum + ")";
+    document.getElementById('cart-item-count').innerHTML = "(" + cartNum + ")";
 };
 
-function addToCart(){
-	cartNum ++;
-	itemCount();
+function addToCart() {
+    itemCount();
+};
+
+// cart modal
+var modal = document.getElementById('myCart');
+var btn = document.getElementById('cart-button');
+var span = document.getElementsByClassName('close')[0];
+
+btn.onclick = function() {
+    modal.style.display = "block";
+};
+
+span.onclick = function() {
+    modal.style.display = "none";
 };
