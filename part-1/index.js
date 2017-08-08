1f2d5702d5c629e04e0e42fd8f6541733a3ac074
Roel Cabrera <roel.cabrera@gmail.com>
2017-08-08 13:11:53 -0700

    charger broke last night working on fixing output for second portion of part-1

--- part-1/index.js
+++ part-1/index.js
@@ -1,20 +1,35 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
 
 daysOfWeek = { monday: 1, tuesday: 2, wednesday: 3, thursday: 4, friday: 5, saturday: 6, sunday: 7 };
-
-
 
 app.get('/api/days/:day', (req, res) => {
     let day = req.params.day;
     let num = daysOfWeek[day];
     if (num != undefined) {
-    	console.log(num)
         res.status(200).send('' + num);
     } else {
-    	res.status(400).send(day + ' is not a day of the week');
+        res.status(400).send(day + ' is not a day of the week');
     }
 });
+
+app.post('/api/array/concat', (req, res) => {
+    let arr = [];
+
+    const vals = Object.keys(req.body).map(key => req.body[key]);
+console.log(vals)
+    for (let i = 0; i < vals.length; i++) {
+    	arr.push(vals)
+    }
+    res.status(200).send(arr)
+})
 
 app.listen(3000, () => {
     console.log('app is currently running on localhost:3000');
